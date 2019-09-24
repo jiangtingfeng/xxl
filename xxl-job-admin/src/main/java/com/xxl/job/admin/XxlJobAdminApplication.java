6ae541eeb2843d111572e603e94caa1cc7949438
jiang <jtf@qq.com>
2019-09-24 12:20:21 +0800

    info: zhengchang

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class XxlJobAdminApplication {
-
 	public static void main(String[] args) {
         SpringApplication.run(XxlJobAdminApplication.class, args);
 	}
-
 }
--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/XxlJobExecutorApplication.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/XxlJobExecutorApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class XxlJobExecutorApplication {
-
 	public static void main(String[] args) {
         SpringApplication.run(XxlJobExecutorApplication.class, args);
 	}
-
 }
